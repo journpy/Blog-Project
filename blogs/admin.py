e03203aa5fd3d70a03339f5616203175d40892bb
Uchenna Adubasim <uchadubs@yahoo.com>
2025-07-17 19:47:07 +0100

    updates to blog project.

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,11 +4,8 @@ from .models import Blog
 
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'content', 'date_created']
+    list_display = ['id', 'title', 'author', 'content', 'date_created']
     search_fields = ('title',)
     list_filter = ['author']
-
-
-
 
 admin.site.register(Blog, BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User as UserModel
+
+
 class Blog(models.Model):
     title = models.CharField()
-    author = models.CharField()
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['-date_created']
 
 
 
